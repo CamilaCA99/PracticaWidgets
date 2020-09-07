e575d3dfe5891f6116193d57d07d3b4b545ea4ad
CamilaCA99 <camicastroarratia@gmail.com>
2020-09-07 13:14:41 -0300

    Segundo Commit

--- app/src/main/java/com/castro/practicawidgets/MainActivity.java
+++ app/src/main/java/com/castro/practicawidgets/MainActivity.java
@@ -3,12 +3,36 @@ package com.castro.practicawidgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText ptNombre;
+    private EditText ptApellido;
+    private RadioGroup RadioGroup;
+    private Button btnEnviar;
+    private RatingBar ratingBar;
+    private CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ptNombre = findViewById(R.id.ptNombre);
+        ptApellido = findViewById(R.id.ptApellido);
+        ptNombre = findViewById(R.id.ptNombre);
+        RadioGroup = findViewById(R.id.RadioGroup);
+        btnEnviar = findViewById(R.id.btnEnviar);
+        ratingBar = findViewById(R.id.ratingBar);
+        checkBox = findViewById(R.id.cbSi);
+        checkBox = findViewById(R.id.cbNo);
+        checkBox = findViewById(R.id.cbNotengo);
+        //commit
+
     }
 }
